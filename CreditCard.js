9c3b84920622b3b4d24577d4e6bab0a2acf69877
Kuka <martinelliluca@gmail.com>
2021-01-15 15:45:57 -0300

    agregue un programa para pagar con tarjeta de credito

---
+++ CreditCard.js
@@ -0,0 +1,85 @@
+
+console.log('Price: $', Coins1, ' ', 'Quantity of coins:', coins1);
+console.log('Price: $', Coins2, ' ', 'Quantity of coins:', coins2);
+console.log('Price: $', Coins3, ' ', 'Quantity of coins:', coins3);
+console.log('Price: $', Coins4, ' ', 'Quantity of coins:', coins4);
+console.log('Price: $', Coins5, ' ', 'Quantity of coins:', coins5);
+console.log('Price: $', Coins6, ' ', 'Quantity of coins:', coins6);
+
+let Credit = 'Credit Card';
+
+
+Credit = 500000;
+
+
+switch (Credit) {
+  case ('Credit Card'):
+      break;
+}
+
+
+
+cOins1 = 100000
+cOins2 = 80000
+cOins3 = 20000
+cOins4 = 5000
+cOins5 = 500
+cOins6 = 100
+cOins7 = 50
+
+
+ COins1 = 10000
+ COins2 = 8000
+ COins3 = 5000
+ COins4 = 1000
+ COins5 = 200
+ COins6 = 120
+ COins7 = 70
+
+
+
+let CoInS1 = COins1 + cOins1;
+ 
+let CoInS2 = COins2 + cOins2;
+
+let CoInS3 = COins3 + cOins3;
+
+let CoInS4 = COins4 + cOins4;
+
+let CoInS5 = COins5 + cOins5;
+
+let CoInS6 = COins6 + cOins6;
+
+
+
+
+
+let  buy = CoInS1 + cOins1;
+let  Buy = CoInS2 + cOins2;
+let  BUy = CoInS3 + cOins3;
+let  pay = CoInS4 + cOins4;
+let  Pay = CoInS5 + cOins5;
+let  PAy = CoInS6 + cOins6;
+
+
+console.log('')
+
+
+let  subtract = CoInS1 - cOins1;
+let  less = CoInS2 - cOins2;
+let  Less = CoInS3 - cOins3;
+let  LEss = CoInS4 - cOins4;
+let  LESs = CoInS5 - cOins5;
+let  lESS = CoInS6 - cOins6;
+
+
+if (Credit >= CoInS1) {  
+        console.log('Transaction ¡Ok! Now you have : $', subtract);
+} else {
+  console.log('You cant pay for this product');
+}
+
+
+Credit = subtract;
+
+//console.log(Credit);
