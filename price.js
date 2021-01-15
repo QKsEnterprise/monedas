3158e85eed7b48350e5a28a21838ce1409842942
Kuka <martinelliluca@gmail.com>
2021-01-15 12:34:23 -0300

    agregue un programa para comprar monedas

---
+++ price.js
@@ -0,0 +1,41 @@
+
+  coins1 = 100000
+  coins2 = 80000
+  coins3 = 20000
+  coins4 = 5000
+  coins5 = 500
+  coins6 = 100
+  coins7 = 50
+
+
+   Coins1 = 10000
+   Coins2 = 8000
+   Coins3 = 5000
+   Coins4 = 1000
+   Coins5 = 200
+   Coins6 = 120
+   Coins7 = 70
+
+ 
+
+
+ let COINS1 = Coins1 + coins1;
+ 
+ let COINS2 = Coins2 + coins2;
+
+ let COINS3 = Coins3 + coins3;
+ 
+ let COINS4 = Coins4 + coins4;
+
+ let COINS5 = Coins5 + coins5;
+ 
+ let COINS6 = Coins6 + coins6;
+ 
+
+
+console.log('Price: $', Coins1, ' ', 'Quantity of coins:', coins1);
+console.log('Price: $', Coins2, ' ', 'Quantity of coins:', coins2);
+console.log('Price: $', Coins3, ' ', 'Quantity of coins:', coins3);
+console.log('Price: $', Coins4, ' ', 'Quantity of coins:', coins4);
+console.log('Price: $', Coins5, ' ', 'Quantity of coins:', coins5);
+console.log('Price: $', Coins6, ' ', 'Quantity of coins:', coins6);
